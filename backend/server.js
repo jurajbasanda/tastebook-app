c30e5a0650897ac2dfa9fce135ae2b3b75f8b2e6
Juraj Basanda <basanda.juraj@gmail.com>
2021-04-27 19:25:04 +0100

    up

--- backend/server.js
+++ backend/server.js
@@ -12,19 +12,29 @@ const app = express()
 //Middleware
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send('Home screen of server')
-})
 app.use('/api/recipes', recipe)
 app.use('/api/users', user)
 app.use('/api/upload', upload)
+//production
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')))
+	app.get('*',(req,res)=>{
+		res.sendFile(path.resolve(__dirname,'/frontend/build/index.html'))
+	})
+} else {
+	app.get('/', (req, res) => {
+		res.send('Home screen of server')
+	})
+}
 //Set PORT
 const PORT = process.env.PORT || 5000
 //Start server
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-			.inverse
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.inverse
 	)
 )
